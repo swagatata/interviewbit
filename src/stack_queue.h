3b3641f11e54908699d3647dde22ccbc54b53b85
swagat <swagatata@gmail.com>
2018-07-14 19:27:21 +0530

    + rain-water-trapped

--- src/stack_queue.h
+++ src/stack_queue.h
@@ -175,4 +175,108 @@ void test_largestRectangleArea() {
 
 	cout << "Test 2 "<< (largestRectangleArea(v) == 3 ? "Passed" : "Failed") << endl;
 }
+
+// assum 1 : distinct elements
+vector<int> slidingMaximum(const vector<int> &v, int w) {
+	if (w > v.size()) {
+		vector<int> ve;
+		ve.push_back(*(max_element(v.begin(), v.end())));
+
+		return ve;
+	}
+
+	deque<pair<int, int> > dq;
+	dq.push_back(make_pair(v[0], 0));
+
+	for (int i = 1; i < w; ++i) {
+		while (dq.size() > 0 && v[i] >= dq.back().first) {
+			dq.pop_back();
+		}
+		dq.push_back(make_pair(v[i], i));
+	}
+
+	vector<int> max;
+	max.push_back(dq.front().first);
+
+	for (size_t wend = w; wend < v.size(); ++wend) {
+		if (wend - w == dq.front().second) {
+			dq.pop_front();
+		}
+
+		while (dq.size() > 0 && v[wend] >= dq.back().first) {
+			dq.pop_back();
+		}
+		dq.push_back(make_pair(v[wend], wend));
+
+		max.push_back(dq.front().first);
+	}
+
+	return max;
+}
+
+void testslidingMaximum() {
+	int a[5] = {3, 2, 1, 5, 4};
+	vector<int> v(a, a+5);
+	vector<int> act;// = slidingMaximum(v, 2);
+
+	int b[4] = {3, 2, 5, 5};
+	vector<int> exp(b, b+4);
+
+	cout << "Test 1 "<< (compare_vectors(act, exp) ? "Passed" : "Failed") << endl;
+
+	v.resize(1);
+	v[0] = 3;
+	act = slidingMaximum(v, 2);
+	cout << "Test 2 "<< (compare_vectors(act, v) ? "Passed" : "Failed") << endl;
+
+	int at[50] = {648, 614, 490, 138, 657, 544, 745, 582, 738, 229, 775, 665, 876, 448, 4, 81, 807, 578, 712, 951, 867, 328, 308, 440, 542, 178, 637, 446, 882, 760, 354, 523, 935, 277, 158, 698, 536, 165, 892, 327, 574, 516, 36, 705, 900, 482, 558, 937, 207, 368};
+	vector<int> vt(at, at+50);
+	act = slidingMaximum(vt, 9);
+	int bt[] = {745,745,775,775,876,876,876,876,876,876,876,951,951,951,951,951,951,951,951,951,882,882,882,882,935,935,935,935,935,935,935,935,935,892,892,892,900,900,900,937,937,937};
+	vector<int> expt(bt, bt+42);
+
+	cout << "Test 3 "<< (compare_vectors(act, expt) ? "Passed" : "Failed") << endl;
+}
+
+int rainWaterTrapped(const vector<int> &A) {
+	if (A.size() < 3) return 0;
+
+	int n = A.size();
+	vector<int> leftmax(n);
+	vector<int> rightmax(n);
+	leftmax[0] = A[0];
+	rightmax[n - 1] = A.back();
+	for (int i = 1; i < n; ++i) {
+		if (A[i] >= leftmax[i - 1])
+			leftmax[i] = A[i];
+		else
+			leftmax[i] = leftmax[i-1];
+
+		if (A[n - i - 1] >= rightmax[n - i])
+			rightmax[n - i - 1] = A[n - i - 1];
+		else
+			rightmax[n - i - 1] = rightmax[n - i];
+	}
+
+//	pv(leftmax);
+//	pv(rightmax);
+
+	unsigned long long area = 0;
+	for (int i = 1; i < A.size() - 1; ++i) {
+		int edge = min(leftmax[i-1], rightmax[i+1]);
+		if (edge > A[i]) {
+			area += (edge - A[i]);
+		}
+	}
+
+	return area;
+}
+
+void testrainWaterTrapped() {
+	int a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+	vector<int> v(a, a+12);
+
+	cout << "Test 1 "<< (rainWaterTrapped(v) == 6 ? "Passed" : "Failed") << endl;
+}
+
 #endif /* SRC_STACK_QUEUE_H_ */
