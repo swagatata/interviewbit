7804b86004801746c6c811dc88aa3287d376f369
swagat <swagatata@gmail.com>
2018-07-18 22:50:54 +0530

    + subset_sum

--- src/dp.h
+++ src/dp.h
@@ -50,11 +50,73 @@ ull nth_ugly_number(ui n) {
 	return UN[n-1];
 }
 
+
+
 void test_nth_ugly_number() {
 
 	cout << "(Test 1) "<< (nth_ugly_number(1) == 1  ? "Passed" : "Failed") << endl;
 	cout << "(Test 2) "<< (nth_ugly_number(4) == 4  ? "Passed" : "Failed") << endl;
 	cout << "(Test 3) "<< (nth_ugly_number(10) == 12  ? "Passed" : "Failed") << endl;
+}
+
+ull combinations(ui n, ui k) {
+	ui ** c;
+	c = new ui*[n+1];
+	FOR(i, (n+1)) {
+		c[i] = new ui[k+1];
+		c[i][0] = 1;
+	}
+	FOR(i, (k+1)) {
+		c[0][i] = 0;
+	}
+
+	c[0][0] = 1;
+	for (ui i = 1; i <= n; ++i) {
+		for (ui j = 1; j <= min(i,k); ++j) {
+			if (i == j) {
+				c[i][j] = 1;
+			} else {
+				c[i][j] = c[i-1][j] + c[i-1][j-1];
+			}
+			cout << "c[" << i << "][" << j << "] is " << c[i][j] << endl;
+		}
+	}
+
+	return c[n][k];
+}
+
+void test_combinations() {
+	combinations(10, 4);
+}
+
+bool subset_sum(vector<int> v, ull sum) {
+	bool * ss;
+
+	ss = new bool[sum+1];
+
+	ss[0] = true;
+	for (ui i = 1; i <= sum; ++i) {
+		ss[i] = false;
+	}
+
+	for (ui j = 1; j <= v.size(); ++j) {
+		for (int i = 1; i <= sum; ++i) {
+			ss[i] = ss[i] || ((i - v[j-1]) >= 0 ? ss[i - v[j-1]] : false);
+		}
+	}
+
+	return ss[sum];
+}
+
+void test_subset_sum() {
+	int a[] = {3, 34, 4, 12, 5, 2};
+	vector<int> v(a, a + 6);
+
+	cout << "(Test 1) "<< (subset_sum(v, 9)  ? "Passed" : "Failed") << endl;
+
+	v.clear();
+	cout << "(Test 2) "<< (!subset_sum(v, 9)  ? "Passed" : "Failed") << endl;
+	cout << "(Test 3) "<< (subset_sum(v, 0)  ? "Passed" : "Failed") << endl;
 }
 
 #endif /* SRC_DP_H_ */
